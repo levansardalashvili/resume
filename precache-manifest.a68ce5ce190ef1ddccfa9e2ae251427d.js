d14ea3950b00d1bdbd413c936505863cde7273e5
levan sardalashvili <sardalaalevani@gmail.com>
2022-02-05 11:59:47 +0400

    Updates

---
+++ precache-manifest.a68ce5ce190ef1ddccfa9e2ae251427d.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc00b5dc06d9b11771be9b7b3fbab43c",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "9f5dfbcfeae727be5e67",
+    "url": "/resume/static/css/main.a8e0a562.chunk.css"
+  },
+  {
+    "revision": "d4605827cebed657f13d",
+    "url": "/resume/static/js/2.1794d81b.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/resume/static/js/2.1794d81b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f5dfbcfeae727be5e67",
+    "url": "/resume/static/js/main.a8d05688.chunk.js"
+  },
+  {
+    "revision": "2b17229f8e36d952a251",
+    "url": "/resume/static/js/runtime-main.6275bf14.js"
+  },
+  {
+    "revision": "8951ec522d3d70c9c13d791faa58e761",
+    "url": "/resume/static/media/avatar.8951ec52.svg"
+  },
+  {
+    "revision": "91abef0b790d7e2b075becfd0a0be784",
+    "url": "/resume/static/media/build-house.91abef0b.JPG"
+  },
+  {
+    "revision": "97ef711450eac8e1bc2b3a0b1a8eeaa6",
+    "url": "/resume/static/media/cars.97ef7114.JPG"
+  },
+  {
+    "revision": "ca042db5d0a17f1f6ee36ea90fa2f3a4",
+    "url": "/resume/static/media/computer.ca042db5.svg"
+  },
+  {
+    "revision": "9c9c21e82f7cca1404006ede8add5ecb",
+    "url": "/resume/static/media/css.9c9c21e8.svg"
+  },
+  {
+    "revision": "d0ec26c2a9a03f1d025ec402dfb44eb2",
+    "url": "/resume/static/media/dowload.d0ec26c2.svg"
+  },
+  {
+    "revision": "b4a408c836528e03a27cc0e6b6c20067",
+    "url": "/resume/static/media/food.b4a408c8.JPG"
+  },
+  {
+    "revision": "a6d8c48cdc66316c579ced5a55cb88ac",
+    "url": "/resume/static/media/github.a6d8c48c.svg"
+  },
+  {
+    "revision": "752f04dcf7d58c9c57f0a0dd4a5f24c4",
+    "url": "/resume/static/media/html.752f04dc.svg"
+  },
+  {
+    "revision": "87711407412494d529d45ee76167583c",
+    "url": "/resume/static/media/javascript.87711407.svg"
+  },
+  {
+    "revision": "3282b9eceda89caf1eb0445584f75b18",
+    "url": "/resume/static/media/judo.3282b9ec.JPG"
+  },
+  {
+    "revision": "a0e4574eb510608fc0f5fbe30ffbd01f",
+    "url": "/resume/static/media/location.a0e4574e.svg"
+  },
+  {
+    "revision": "79b094a39dcb7a0676b5e88f69fd5573",
+    "url": "/resume/static/media/movies.79b094a3.JPG"
+  },
+  {
+    "revision": "a8d4d8a9320dea762169a477158b3f92",
+    "url": "/resume/static/media/portfolio.a8d4d8a9.JPG"
+  },
+  {
+    "revision": "5f566b56befde40b3e6fcf72d226ea4b",
+    "url": "/resume/static/media/react.5f566b56.svg"
+  },
+  {
+    "revision": "db7b31a5fd7a28a44f1a0ec45a1468f8",
+    "url": "/resume/static/media/resume.db7b31a5.pdf"
+  },
+  {
+    "revision": "461014d2f5d649ba75fe22b76999ab79",
+    "url": "/resume/static/media/sass.461014d2.svg"
+  },
+  {
+    "revision": "193f99069e4f3aa5615cb002444b9ef3",
+    "url": "/resume/static/media/search.193f9906.JPG"
+  },
+  {
+    "revision": "d8199bf4874cf8a312bc378c63aa95be",
+    "url": "/resume/static/media/travel.d8199bf4.JPG"
+  },
+  {
+    "revision": "9b17f2aaaa7546b6158acdab7629ef27",
+    "url": "/resume/static/media/traveljs.9b17f2aa.JPG"
+  },
+  {
+    "revision": "6283adf5b4a562bca00a53b9cc290c59",
+    "url": "/resume/static/media/weather.6283adf5.jpg"
+  },
+  {
+    "revision": "7da3efce2039a251986d8f770c0d58e5",
+    "url": "/resume/static/media/wine.7da3efce.JPG"
+  }
+]);
